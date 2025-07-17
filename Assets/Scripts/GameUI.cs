9faaee8a34f21270670a645942b63a4b6fd36d75
Adilkhan-111 <gulzhanyessetova@gmail.com>
2025-07-17 09:59:27 +0500

    initial commit
    added basic playable version

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    public Text BestScoreText;
+    private void Awake()
+    {
+        BestScoreText.text = "Best Score: " + GameManager.instance.PlayerName + " : " + GameManager.instance.HighScoreNum;
+    }
+}
